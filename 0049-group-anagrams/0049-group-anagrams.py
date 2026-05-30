e757e5ee92a2d6a7d75a3ad912c95b101d3f0d70
DevManTowhid <mdtowhidulalam364@gmail.com>
2026-05-31 01:41:56 +0600

    Time: 11 ms (84.31%), Space: 21.7 MB (92.97%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.py
+++ 0049-group-anagrams/0049-group-anagrams.py
@@ -1,17 +1,10 @@
-from collections import defaultdict
-
 class Solution:
-    def groupAnagrams(self, strs: list[str]) -> list[list[str]]:
-        # Map will store: { tuple_of_sorted_chars : [list_of_original_strings] }
-        anagram_map = defaultdict(list)
-        
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        ans = {}
         for s in strs:
-            # 1. Sort the string
-            # 2. Convert the resulting list into a tuple (which is hashable)
-            sorted_key = tuple(sorted(s))
-            
-            # 3. Append the original string to the corresponding group
-            anagram_map[sorted_key].append(s)
-            
-        # Return all the grouped lists
-        return list(anagram_map.values())
+            s2 = "".join(sorted(s))
+            if s2 in ans:
+                ans[s2].append(s)
+            else:
+                ans[s2] = [s]
+        return list(ans.values())
