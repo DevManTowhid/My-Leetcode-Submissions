b38c955dc91a5f6b53c4d1e60ebe01865bda54e3
DevManTowhid <mdtowhidulalam364@gmail.com>
2026-06-02 14:59:41 +0600

    Time: 0 ms (100%), Space: 17.8 MB (100%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -0,0 +1,12 @@
+class Solution:
+    def climbStairs(self, n: int) -> int:
+        if n <= 2:
+            return n
+
+        first, second = 1, 2  # ways(1), ways(2)
+
+        for _ in range(3, n + 1):
+            third = first + second
+            first, second = second, third
+
+        return second
