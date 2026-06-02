7eb5131e92a04d8ed87e56a5612e1d39b7d498ab
DevManTowhid <mdtowhidulalam364@gmail.com>
2026-06-02 14:57:03 +0600

    Time: 0 ms (100%), Space: 11.7 MB (26.04%) - LeetHub

--- 0066-plus-one/0066-plus-one.cpp
+++ 0066-plus-one/0066-plus-one.cpp
@@ -1,6 +1,25 @@
+#include <vector>
+#include <algorithm>
+
 class Solution {
 public:
-    vector<int> plusOne(vector<int>& digits) {
+    std::vector<int> plusOne(std::vector<int>& digits) {
+        int n = digits.size();
         
+        // Traverse the digits from the end to the beginning
+        for (int i = n - 1; i >= 0; --i) {
+            if (digits[i] < 9) {
+                // If the digit is less than 9, just increment and return
+                digits[i]++;
+                return digits;
+            }
+            // If the digit is 9, it becomes 0 and we continue to the carry
+            digits[i] = 0;
+        }
+        
+        // If we reach here, it means all digits were 9 (e.g., 999 -> 000)
+        // We need to add a 1 at the beginning
+        digits.insert(digits.begin(), 1);
+        return digits;
     }
 };
